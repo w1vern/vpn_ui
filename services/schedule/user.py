e3cdc35da3aee40c58bcd9ade6e13f9291957ed5
w1vern <bndvvl@gmail.com>
2025-12-02 01:20:53 +0300

    start schedule refactoring

--- services/schedule/user.py
+++ services/schedule/user.py
@@ -33,24 +33,32 @@ async def check_users() -> None:
             async with server_session_manager.get_session(server) as server_session:
                 services.append(Service(db_session, server_session))
         for user in users:
+            # skip verified
             if user.is_verified is False:
                 continue
 
+            # Auto pay
             last_active_periods = await apr.get_last_by_user_id(user)
             current_date = datetime.now(UTC).replace(tzinfo=None)
+            if last_active_periods is None:
+                if user.balance > user.tariff.price and user.auto_pay is True:
+                    await tr.create(
+                        user=user,
+                        amount=user.tariff.price,
+                        description="Auto pay",
+                        date=current_date,
+                        transaction_type=TransactionType.withdrawal.value
+                    )
+                continue
+
             traffic = 0
             for service in services:
                 traffic += await service.get_traffic(user)
 
-            if last_active_periods is None:
-                if user.balance > user.tariff.price and user.auto_pay is True:
-                    await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value) # type: ignore
-                continue
-
             if last_active_periods.result_traffic != -1:
                 if user.balance > user.tariff.price and user.auto_pay is True:
                     await ur.update_rights(user, {Rights.can_use.name: True})
-                    await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value) # type: ignore
+                    await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value)
                     for service in services:
                         await service.set_enable(user, True, None)
                 continue
@@ -60,7 +68,8 @@ async def check_users() -> None:
                 for service in services:
                     await service.reset_traffic(user)
                 if user.auto_pay is True and user.balance > user.tariff.price:
-                    await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value) # type: ignore
+                    # type: ignore
+                    await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value)
                 else:
                     await ur.update_rights(user, {Rights.can_use.name: False})
                     for service in services:
@@ -73,7 +82,8 @@ async def check_users() -> None:
                     for service in services:
                         await service.reset_traffic(user)
                     if user.auto_pay is True and user.balance > user.tariff.price_of_traffic_reset:
-                        trans = await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value) # type: ignore
+                        # type: ignore
+                        trans = await tr.create(user, user.tariff.price, current_date, TransactionType.withdrawal.value)
                         if not trans:
                             raise Exception("Transaction is None")
                         await apr.close_period(last_active_periods, traffic, current_date)
